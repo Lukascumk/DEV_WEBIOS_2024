6c538e8e4414d64cd5be6a264e68d2deac9935fa
Lukas Mota <Lukas_motta@outlook.com>
2024-10-10 21:33:51 -0300

    variaveis

---
+++ JS/JS02-Console/main.js
@@ -0,0 +1,16 @@
+// quando você cria uma variável em JavaScript, precisa usar uma das palavras-chave corretas: var, let ou const.
+
+// var: Declara uma variável que pode ser reatribuída, mas tem escopo de função ou global, dependendo de onde é declarada.
+
+// let: Declara uma variável que também pode ser reatribuída, mas tem escopo de bloco, o que significa que está restrita ao bloco onde foi definida. Let é permitido mudar o valor
+
+// const: Declara uma variável cujo valor não pode ser reatribuído, também com escopo de bloco.
+
+// A palavra-chave function é usada para declarar funções em JavaScript, que são blocos de código que podem ser executados quando chamados. 
+
+
+// valor de var pode ser utilizado varias vezes com valores diferentes diferente de const e let
+
+// um sinal de  = é para atribuir valor a uma variavel.
+
+// var x = 1; ---- var nome = 'HOME' ---- var teste;
