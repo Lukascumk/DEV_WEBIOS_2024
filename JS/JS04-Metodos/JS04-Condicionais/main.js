540ef7168bc70bfa73afa5849693fdb66d4c5c2d
Lukas Mota <Lukas_motta@outlook.com>
2024-10-21 21:42:31 -0300

    Condicionais

---
+++ JS/JS04-Metodos/JS04-Condicionais/main.js
@@ -0,0 +1,33 @@
+// (=)  atribuição
+// (==) comparação true == true, noite == dia , etc.
+// (===) faz uma terceira verificação, verificação de comparação de dados e comparação de tipos
+
+let estaComBlusa = true;
+
+if(estaComBlusa == true ){
+    console.log(`pode assistir aula normalmente, seu zé ruela`)
+}
+
+console.clear();
+
+let pontos = 500;
+
+if (pontos >=1000){
+    console.log(`Parabéns! Você passou para a faze 2!`);
+}
+
+else {
+    console.log(`Você é muito Ruim, vá estudar mais`);
+}
+
+console.clear()
+
+let imparPar = 235698798
+
+if (imparPar % 2 == 0) {
+    console.log(`Esse Número é PAR`);
+}
+
+else{
+    console.log(`Esse Número é Impar`);
+}
