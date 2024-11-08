286456a53f1a8399fee79ce78461dc5de191c377
Lukas Mota <Lukas_motta@outlook.com>
2024-11-07 21:47:23 -0300

    array

--- JS/JS07- ExercicioCasa/main.js
+++ JS/JS07- ExercicioCasa/main.js
@@ -5,33 +5,33 @@ let alunos =
         , idade:17
         , nota: 8
         , ano: '2ºB'
-        }
+        },
 
     {
         nome:'Bruno'
         , idade: 16
         , nota:6
         , ano:'2ºC'
-    }
+    },
 
     {
         nome:'Veronica'
         , idade:16
         , nota:6
         , ano:'2ºC'
-    }
+    },
     {
         nome:'Marta'
         , idade:15
         , nota:5
         , ano:'3ºC'
-    }
+    },
     {
         nome:'Brenno'
         , idade:19
         , nota:6
         , ano:'3ºC'
-    }
+    },
     {
         nome:'Maria'
         , idade:14
--- JS/JS09-MetodoArray/main.js
+++ JS/JS09-MetodoArray/main.js
@@ -1,24 +1,42 @@
-let frutas = [ 
-    "maça", 
-    "banana", 
-    "abacaxi", 
-    "manga", 
-    "melancia", 
-    "uva", 
-    "kiwi", 
+let frutas = [
+    "maça",
+    "banana",
+    "abacaxi",
+    "manga",
+    "melancia",
+    "uva",
+    "kiwi",
     "amora",
 ]
 
+
+let infoUsuraio = [
+    ["Ana", 1234],
+    ["Bruno", 3214]
+    ["João", 5634]
+]
+
+
+infoUsuraio.forEach((valor,[indice]) =>{
+    console.log(valor,indice);
+})
+frutas.forEach((valor, indice) => {
+
+    console.log([1][1]);
+    
+})
+
+
 // for (i = 0; i <= frutas.length ; i++){
 
-    //      console.log(i) //numero da posição
-    //     console.log(frutas[i]); //valor da posição
+//      console.log(i) //numero da posição
+//     console.log(frutas[i]); //valor da posição
 // }
 
-frutas.forEach(Percorrer)
-//primeiro é referente ao valor do conteudo
-//segundo parametro é referente ao indice
-function Percorrer(valor, indice){
-    console.log(valor);
-    console.log(indice);
-}
+// frutas.forEach(Percorrer)
+// //primeiro é referente ao valor do conteudo
+// //segundo parametro é referente ao indice
+// function Percorrer(valor, indice){
+//     console.log(valor);
+//     console.log(indice);
+// }
