1acf5711e96e143f40629e8dc0834d3b4fefe1ef
Lukas Mota <Lukas_motta@outlook.com>
2024-11-08 20:51:42 -0300

    metodos de pesquisa de array

--- JS/JS09-MetodoArray/main.js
+++ JS/JS09-MetodoArray/main.js
@@ -1,30 +1,30 @@
-let frutas = [
-    "maça",
-    "banana",
-    "abacaxi",
-    "manga",
-    "melancia",
-    "uva",
-    "kiwi",
-    "amora",
-]
+// let frutas = [
+//     "maça",
+//     "banana",
+//     "abacaxi",
+//     "manga",
+//     "melancia",
+//     "uva",
+//     "kiwi",
+//     "amora",
+// ]
 
 
-let infoUsuraio = [
-    ["Ana", 1234],
-    ["Bruno", 3214]
-    ["João", 5634]
-]
+// let infoUsuraio = [
+//     ["Ana", 1234],
+//     ["Bruno", 3214]
+//     ["João", 5634]
+// ]
 
 
-infoUsuraio.forEach((valor,[indice]) =>{
-    console.log(valor,indice);
-})
-frutas.forEach((valor, indice) => {
+// infoUsuraio.forEach((valor,[indice]) =>{
+//     console.log(valor,indice);
+// })
+// frutas.forEach((valor, indice) => {
 
-    console.log([1][1]);
+//     console.log([1][1]);
     
-})
+// })
 
 
 // for (i = 0; i <= frutas.length ; i++){
@@ -40,3 +40,69 @@ frutas.forEach((valor, indice) => {
 //     console.log(valor);
 //     console.log(indice);
 // }
+
+
+
+
+// Aula 09/2
+
+
+//Math.currentyl / Math.max / max.min / math.sqrt
+
+let numeros = [
+    20, 100, 30, 40,2056,65879
+]
+//trazer um novo array atualizado com a raiz quadrada de cada numero do array
+
+let numerosDois = numeros.map(Math.sqrt)
+let MaiorNumero = Math.max.apply(Math, numeros)
+let menorNumero = Math.min.apply(Math,numeros)
+
+
+console.log(`maior` +MaiorNumero + "      " + `menor` + menorNumero)
+console.log( `maior`+MaiorNumero)
+console.log(numeros)
+console.log(numerosDois)
+
+
+
+// aplicando metodo filter
+console.clear()
+
+
+let filtrar = numeros.filter((valor) => {
+    return valor>= 1000; 
+})
+
+console.log(`Array completo:  `+numeros)
+console.log(`Array com filtro:  `+filtrar)
+
+
+//metodo find() achar ou encontrar
+
+
+console.clear()
+
+
+let tarefa = [
+    {
+        tarefa: `Dormir`,
+        isFeita: true
+    },
+    {
+        tarefa:`Limpar`,
+        isFeita: false
+    },
+    {
+        tarefa:`torcer`,
+        isFeita: true
+    },
+]
+
+//retorne apenas as tarefas true utilizando find
+
+
+let retornaTrue = tarefa.find((item)=>{
+    return item.isFeita == true
+    console.log(tarefa)    
+})
