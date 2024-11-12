8e879c63fe2fbc1b7df6b53a14601198491fb2dd
Lukas Mota <Lukas_motta@outlook.com>
2024-11-12 19:52:40 -0300

    finalizado

--- JS/JS10-Classes/main.js
+++ JS/JS10-Classes/main.js
@@ -19,13 +19,24 @@ class Aluno {
         this.cor = cor;
     }
 
-retornaIdade(){ 
+    retornaIdade(ano) {
+        let dataCompleta = new Date();
+        let pegaAno = dataCompleta.getFullYear() 
+        return pegaAno - this.ano
 
+
+    }
 }
-}
+
+
+// objeto no JS Date -- retorna informações de data de acordo com oque está configurado no seu computador/servidor
+
+
 //instancia == cópias
 let aluno1 = new Aluno("Lukas", 1997, 1.80, true, true, "Papelão molhado");
 let aluno2 = new Aluno("Jeff", 2005, 1.75, true, true, "AllWhite")
 // console.log(aluno1);
 console.log("Segundo Aluno:", aluno1, "Segundo Aluno:", aluno2)
-console.log(aluno1.nome, aluno1.idade)
+console.log(aluno1.nome, aluno.ano)
+
+console.log(`A idade do aluno${aluno1} é: ${retornaIdade()}`);
